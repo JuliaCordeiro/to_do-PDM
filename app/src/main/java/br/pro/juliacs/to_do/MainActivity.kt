a0b3693c48d33693e4a148b4470c8df5b852edd6
Júlia Cordeiro <julia.c2001@hotmail.com>
2022-05-05 10:17:44 -0300

    Insert functions to MainActivity

--- app/src/main/java/br/pro/juliacs/to_do/MainActivity.kt
+++ app/src/main/java/br/pro/juliacs/to_do/MainActivity.kt
@@ -2,10 +2,57 @@ package br.pro.juliacs.to_do
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Switch
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import br.pro.juliacs.to_do.models.TaskData
+import br.pro.juliacs.to_do.ui.list.TaskDataAdapter
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var etxtDescription: EditText
+    private lateinit var tasksList: ArrayList<TaskData>
+    private lateinit var rvTaskData: RecyclerView
+    private lateinit var taskDataAdapter: TaskDataAdapter
+    private lateinit var swtUrgent: Switch
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.etxtDescription = findViewById(R.id.etxtDescription)
+        this.rvTaskData = findViewById(R.id.rvTaskData)
+        this.swtUrgent = findViewById(R.id.swtUrgent)
+        if(savedInstanceState == null)
+            this.tasksList = ArrayList()
+        else if(savedInstanceState.containsKey("RECOVER_TASK")) {
+            this.tasksList =
+                savedInstanceState.getParcelableArrayList<TaskData>("RECOVER_TASK")
+                as ArrayList<TaskData>
+        }
+        this.taskDataAdapter = TaskDataAdapter(this.tasksList)
+        this.rvTaskData.layoutManager = LinearLayoutManager(this)
+        this.rvTaskData.adapter = this.taskDataAdapter
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelableArrayList("RECOVER_TASK", this.tasksList)
+    }
+
+    fun onClickSend(v: View) {
+        val taskText = this.etxtDescription.text.toString()
+        if(taskText.isNotBlank()) {
+            val task = TaskData(
+                taskText,
+                this.swtUrgent.isChecked,
+                false
+                )
+            this.tasksList.add(task)
+            this.taskDataAdapter.notifyItemInserted(this.tasksList.size-1)
+            this.etxtDescription.text.clear()
+
+        }
+    }
+
 }
