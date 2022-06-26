627fc57849b41c4e2a4087c98760ac81b474580c
Júlia Cordeiro <julia.c2001@hotmail.com>
2022-06-25 23:58:34 -0300

    Insert task creator elements on main screen bottom bar

--- app/src/main/java/br/pro/juliacs/to_do/ui/screens/MainScreenView.kt
+++ app/src/main/java/br/pro/juliacs/to_do/ui/screens/MainScreenView.kt
@@ -1,20 +1,87 @@
 package br.pro.juliacs.to_do.ui.screens
 
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Scaffold
-import androidx.compose.runtime.Composable
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
-import br.pro.juliacs.to_do.data.TasksSingleton
+import androidx.compose.ui.unit.dp
+import br.pro.juliacs.to_do.data.*
 import br.pro.juliacs.to_do.ui.components.CardView
 import br.pro.juliacs.to_do.ui.theme.ToDoAppTheme
 
 @Composable
-fun MainScreenView() {
-    Scaffold() {
-        LazyColumn {
-            items(TasksSingleton.getTasks()) { task ->
-                CardView(taskData = task)
+fun MainScreenView( /*taskRequest: TaskRequest*/ ) {
+    var newTaskDescription = remember { mutableStateOf(TextFieldValue("")) }
+    var isUrgent = remember { mutableStateOf(false) }
+    val stateTasksList = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            Column {
+                Row(
+                    modifier = Modifier
+                        .align(Alignment.Start)
+                        .padding(horizontal = 5.dp)
+                ) {
+                    Text(
+                        "Is Urgent?",
+                        modifier = Modifier
+                            .align(Alignment.CenterVertically)
+                            .padding(start = 10.dp, end = 5.dp)
+                    )
+                    Switch(
+                        checked = isUrgent.value,
+                        onCheckedChange = {
+                            isUrgent.value = !isUrgent.value
+                        },
+                        modifier = Modifier.align(Alignment.CenterVertically)
+                    )
+                }
+                Row(
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                ) {
+                    TextField(
+                        value = newTaskDescription.value,
+                        onValueChange = { newDescription -> newTaskDescription.value = newDescription},
+                        placeholder = { Text( "Type the task description ") },
+                        modifier = Modifier.padding(10.dp)
+                    )
+                    Button(
+                        onClick = {
+                            Log.d("Button", "click")
+                            // TODO
+                            /*
+                            REQUEST TO INSERT NEW TASKS
+                            */
+                            newTaskDescription.value = TextFieldValue("")
+                        },
+                        Modifier
+                            .align(Alignment.CenterVertically)
+                            .width(80.dp)
+                            .padding(10.dp)
+                    ) {
+                        Text( "OK" )
+                    }
+                }
+            }
+        }
+    ) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+            Column {
+                LazyColumn (state = stateTasksList) {
+                    items(TasksSingleton.getTasks()) { task ->
+                        CardView(taskData = task)
+                    }
+                }
             }
         }
     }
